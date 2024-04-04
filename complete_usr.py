00c8bb1dc4df29a204b530fbf307d4d676fd502a
riyatomar <riyatomar912@gmail.com>
2024-04-04 13:45:31 +0530

    genearte complete USR

---
+++ complete_usr.py
@@ -0,0 +1,25 @@
+import os
+
+
+scripts=[
+    "concept_row_module.py",
+    "indexing_module.py", 
+    "sem_cat_module.py",
+    "morpho_sem_module.py", 
+    "dependency_module.py",
+    "discourse_module.py",
+    "spk_view_module.py", 
+    "scope_module.py",
+    "sent_type_module.py",
+    "construction_module.py"
+]
+
+def run_script(script_name):
+    try:
+        command=f"python3 {script_name}"
+        os.system(command)
+    except Exception as e:
+        print(f"Error running script: {e}")
+
+for script in scripts:
+    run_script(script)
