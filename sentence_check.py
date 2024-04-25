90f7a69245cc902e156a7ef6de576712d486d304
riyatomar <riyatomar912@gmail.com>
2024-04-25 10:34:33 +0530

    updated

--- sentence_check.py
+++ sentence_check.py
@@ -1,59 +1,59 @@
 import os
-def check_punctuation(sentence):
-    flag=0
-    list_punc_with_space=[" |"," ?"," !"," ।"," ."]
-    list_punc_without_space=["|","?","!","।"]
-    
-    
-    if sentence.endswith(tuple(list_punc_with_space))==True:
-        flag=1
-        length=len(sentence)
-        last_word=sentence[-1]
-        if last_word=="।" or last_word=="|":
-            sentence=sentence.replace(last_word,".")
-    elif sentence.endswith(tuple(list_punc_without_space))==True: 
-        flag=1
-        length=len(sentence)
-        last_word=sentence[-1]
-        if last_word=="।" or last_word=="|":
-            sentence=sentence.replace(last_word," "+".")
-        else:
-            sentence=sentence.replace(last_word," "+last_word)
-    elif sentence.endswith(tuple("."))==True:
-        flag=1
-        sentence=sentence.replace("."," "+".")
-    elif sentence.endswith(tuple(" ."))==True:
-        flag=1
-        sentence=sentence.replace(".",".")
 
-    if flag==0:
-        sentence=sentence+" ."
-    
+def check_punctuation(sentence):
+    flag = 0
+    list_punc_with_space = [" |", " ?", " !", " ।", " ."]
+    list_punc_without_space = ["|", "?", "!", "।"]
+
+    if sentence.endswith(tuple(list_punc_with_space)):
+        flag = 1
+        last_word = sentence[-1]
+        if last_word == "।" or last_word == "|":
+            sentence = sentence.replace(last_word, ".")
+    elif sentence.endswith(tuple(list_punc_without_space)):
+        flag = 1
+        last_word = sentence[-1]
+        if last_word == "।" or last_word == "|":
+            sentence = sentence.replace(last_word, " " + ".")
+        else:
+            sentence = sentence.replace(last_word, " " + last_word)
+    elif sentence.endswith(tuple(".")):
+        flag = 1
+        sentence = sentence.replace(".", " " + ".")
+    elif sentence.endswith(tuple(" .")):
+        flag = 1
+        sentence = sentence.replace(".", ".")
+
+    if flag == 0:
+        sentence += "."
 
     return sentence
+
 def remove_allpunc(sentence):
-    # define punctuation
-    punctuations = [",","#","<",">","(",")",":",";","-","%","'",'"',"’","‘","*","&","@"]
+    punctuations = [",", "#", "<", ">", "(", ")", ":", ";", "-", "%", "'", '"', "’", "‘", "*", "&", "@"]
     for sym in punctuations:
         if sym in sentence:
-            # print(sentence)
-            sentence=sentence.replace(sym," ")
+            sentence = sentence.replace(sym, " ")
     return sentence
 
-if __name__=="__main__":
-    file_name_input="txt_files/bh-1"
-    file_name_output="txt_files/bh-1"
-    #file_name_input="tmp_files/temp1.txt"
-    #file_name_output="tmp_files/temp1.txt"
-    
-    with open(file_name_input,"r",encoding="UTF-8") as f:
-        sentence=f.readline()
-        sentence=sentence.strip()
-        #print(sentence)
-        #sentence=remove_allpunc(sentence)
-        sentence=check_punctuation(sentence)
-        sentence=remove_allpunc(sentence)
+def remove_starting_connectives(sentence):
+    SIMPLE_CONNECTIVES = ['और', 'एवं', 'इसलिए', 'क्योंकि', 'जबकि', 'तथा', 'ताकि', 'मगर', 'लेकिन', 'किंतु', 'परंतु', 'फिर भी', 'या', 'तथापि',
+                      'नहीं तो', 'व', 'चूंकि', 'चूँकि', 'वरना', 'अन्यथा', 'बशर्तें', 'हालाँकि', 'इसीलिये', 'इसीलिए' , 'इसलिए', 'अथवा', 'अतः', 'अर्थात्', 'जब', 'तो', 'परन्तु', 'कि', 'बल्कि', 'पर']
+    first_word = sentence.split()[0]
+    if first_word in SIMPLE_CONNECTIVES:
+        sentence = " ".join(sentence.split()[1:])
+    return sentence
 
-    os.remove(file_name_input)    
-    f=open(file_name_output,"w",encoding="UTF-8")
-    f.write(sentence)
+if __name__ == "__main__":
+    file_name_input = "txt_files/bh-1"
+    file_name_output = "txt_files/bh-1"
+    
+    with open(file_name_input, "r", encoding="UTF-8") as f:
+        sentence = f.readline().strip()
+        sentence = remove_allpunc(sentence)
+        sentence = check_punctuation(sentence)
+        sentence = remove_starting_connectives(sentence)
+
+    os.remove(file_name_input)
+    with open(file_name_output, "w", encoding="UTF-8") as f:
+        f.write(sentence)
