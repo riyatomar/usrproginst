3081070f1769a2352d3077105fde67c3aa49c33e
riyatomar <riyatomar912@gmail.com>
2024-05-27 10:17:51 +0530

    updated morph code

--- morph_call-1.py
+++ morph_call-1.py
@@ -1,5 +1,4 @@
 import os
-#import subprocess
 
 input_folder = "txt_files/"
 sentence_file = "wx.txt"
@@ -7,37 +6,36 @@ single_concept_inp = "concept.txt"
 single_concept_out = "concept.txt-out.txt"
 output_file = "morph-output.txt"
 
-input_file_path = os.path.join(input_folder,sentence_file)
-output_file_path = os.path.join(input_folder,output_file)
+input_file_path = os.path.join(input_folder, sentence_file)
+output_file_path = os.path.join(input_folder, output_file)
 
-with open(output_file_path,"w") as out:
+with open(output_file_path, "w") as out:
     out.write("")
 
 if os.path.isfile(input_file_path):
-   with open(input_file_path,"r") as inp:
+    with open(input_file_path, "r") as inp:
         sentence = inp.read().strip()
         words = sentence.split()
 
         for word in words:
-        #print(word)
-            with open(single_concept_inp,"w") as sc:
-                sc.write(word)
-                #print(word)
+            # Check if the word is enclosed within angle brackets
+            if word.startswith("<") and word.endswith(">"):
+                with open(output_file_path, "a") as out:
+                    out.write(word + "\n")
+            else:
+                with open(single_concept_inp, "w") as sc:
+                    sc.write(word)
 
-            os.system("sh run_morph-analyser.sh " + single_concept_inp)
-            #subprocess.run(["sh", script, input_folder+"/"+sentence_file])
+                os.system("sh run_morph-analyser.sh " + single_concept_inp)
 
-            with open(single_concept_out,"r") as con_out:
-                output = con_out.read().strip()
+                with open(single_concept_out, "r") as con_out:
+                    output = con_out.read().strip()
 
-            with open(output_file_path,"a") as out:
-                out.write(output + "\n")
+                with open(output_file_path, "a") as out:
+                    out.write(output + "\n")
 
-            with open(single_concept_inp,"w") as sc:
-                sc.write("")
+                with open(single_concept_inp, "w") as sc:
+                    sc.write("")
 
-            with open(single_concept_out,"w") as con_out:
-                con_out.write("")
-
-
-
+                with open(single_concept_out, "w") as con_out:
+                    con_out.write("")
