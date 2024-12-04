d3a7e3dcf196d94fb7f6b879f0c2a7a30636088d
riyatomar <riyatomar912@gmail.com>
2024-12-04 21:31:01 +0530

    Code updated for path

--- run_usrBuilder.py
+++ run_usrBuilder.py
@@ -1,17 +1,17 @@
 import os
 
-# List of commands to execute
+path = '/home/riya/usrproginst/'
 commands = [
-    "python3 run_generate_usr.py",
-    "python3 intermediate_scripts/run_usr.py",
-    "python3 process_usr_files/csv2vertical_format.py txt_files/bulk_USRs_mod/ usr_processed/verticle_USRs",
-    "python3 process_usr_files/merger.py usr_processed/verticle_USRs/ usr_processed/merged_USR",
-    "python3 process_usr_files/add_sent_id.py usr_processed/merged_USR usr_processed/tagged_USRs",
-    "python3 process_usr_files/vertical_usr_segregator.py usr_processed/tagged_USRs usr_processed/vertical_segregated",
-    "python3 complete_cnx.py",
-    "python3 process_usr_files/merge_without_id.py usr_processed/final_outputs usr_processed/usr_file.txt",
-    "python3 process_usr_files/remove_txt.py",
-    "python3 process_usr_files/remove_intermediate_files.py"
+    f"python3 {path}run_generate_usr.py",
+    f"python3 {path}intermediate_scripts/run_usr.py",
+    f"python3 {path}process_usr_files/csv2vertical_format.py {path}txt_files/bulk_USRs_mod/ {path}usr_processed/verticle_USRs",
+    f"python3 {path}process_usr_files/merger.py {path}usr_processed/verticle_USRs/ {path}usr_processed/merged_USR",
+    f"python3 {path}process_usr_files/add_sent_id.py {path}usr_processed/merged_USR {path}usr_processed/tagged_USRs",
+    f"python3 {path}process_usr_files/vertical_usr_segregator.py {path}usr_processed/tagged_USRs {path}usr_processed/vertical_segregated",
+    f"python3 {path}complete_cnx.py",
+    f"python3 {path}process_usr_files/merge_without_id.py {path}usr_processed/final_outputs {path}usr_processed/usr_file.txt",
+    f"python3 {path}process_usr_files/remove_txt.py",
+    f"python3 {path}process_usr_files/remove_intermediate_files.py"
 ]
 
 # Execute commands
